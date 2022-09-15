f2b1bdf7e1041459cf015551c24928662d6e6064
Jiwoon2 <dkswjsgkek@naver.com>
2022-09-15 22:40:35 +0900

    경유 정류소 목록 출력
    노선이 경유하는 정류소 목록 출력

---
+++ HandyBus/app/src/main/java/com/khackathon/handybus/model/StationRouteRepository.java
@@ -0,0 +1,42 @@
+package com.khackathon.handybus.model;
+
+import androidx.lifecycle.MutableLiveData;
+
+import java.util.ArrayList;
+
+public class StationRouteRepository {
+    private static ArrayList<StationRoute_Item> items = new ArrayList<>();
+    private static MutableLiveData<ArrayList<StationRoute_Item>> mutableLiveDataItemsList = new MutableLiveData<>();
+    private static StationRouteRepository stationRouteRepository;
+
+    public StationRouteRepository(){
+        items.clear();
+        mutableLiveDataItemsList.setValue(items);
+    }
+
+    public static StationRouteRepository getStationRouteFirst(){
+        stationRouteRepository= new StationRouteRepository();
+        return stationRouteRepository;
+    }
+
+    //리스트 데이터 초기화
+    public ArrayList<StationRoute_Item> clearStationRoute(ArrayList<StationRoute_Item> list){
+        list.clear();
+        items=list;
+        mutableLiveDataItemsList.postValue(list);
+        return items;
+    }
+
+    public MutableLiveData<ArrayList<StationRoute_Item>> getStationRouteCurrentItem() {
+        return mutableLiveDataItemsList;
+    }
+
+    public ArrayList<StationRoute_Item> getStationRoute(){
+        return items;
+    }
+
+    //갱신
+    public void updateStationRoute(ArrayList<StationRoute_Item> list){
+        mutableLiveDataItemsList.postValue(list);
+    }
+}
